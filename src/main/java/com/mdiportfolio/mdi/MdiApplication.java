1be54b22f66d51a3b50ebdf521f5e25064ff0d10
goldbach8 <marianodeiriondo8@gmail.com>
2023-05-12 12:58:44 -0300

    .

--- src/main/java/com/mdiportfolio/mdi/MdiApplication.java
+++ src/main/java/com/mdiportfolio/mdi/MdiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MdiApplication  {
+public class MdiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdiApplication.class, args);
