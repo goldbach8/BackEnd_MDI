68dc0621d53e3e1207c4d7bfe9ff8c8eb07d9307
goldbach8 <marianodeiriondo8@gmail.com>
2023-05-14 23:14:59 -0300

    .

--- src/main/java/com/mdiportfolio/mdi/Security/Controller/AuthController.java
+++ src/main/java/com/mdiportfolio/mdi/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "https://frontendmdi.web.app/login")
+@CrossOrigin(origins = "https://frontendmdi.web.app")
 public class AuthController  {
     @Autowired
     PasswordEncoder passwordEncoder;
